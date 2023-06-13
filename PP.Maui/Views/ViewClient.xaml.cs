9188e5f859ed5dde01d6fa328142811c99ea470c
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2023-06-13 07:44:39 -0400

    Created page to view client's projects

--- PP.Maui/Views/ViewClient.xaml.cs
+++ PP.Maui/Views/ViewClient.xaml.cs
@@ -6,4 +6,24 @@ public partial class ViewClient : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Create_Project_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Edit_Project_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Delete_Project_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Search_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
