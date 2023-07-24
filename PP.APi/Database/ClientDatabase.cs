37f3f090ce21cd432c79fa434463dab5ac80f34f
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2023-07-24 18:52:10 -0400

    Link Library to API

---
+++ PP.APi/Database/ClientDatabase.cs
@@ -0,0 +1,15 @@
+﻿using PP.Library.Services;
+using PP.Library.Models;
+namespace PP.APi.Database
+{
+    public static class ClientDatabase
+    {
+        //public static List<Client> clients = ClientServices.Current.Clients;
+        public static List<Client> clients = new List<Client>
+        {
+            new Client{Id = 0, Name = "John", Closed = false, Notes="N/A"},
+            new Client{Id = 1, Name = "h", Closed = false, Notes="N/A"},
+            new Client{Id = 2, Name = "g", Closed = false, Notes="N/A"},
+        };
+    }
+}
