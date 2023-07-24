4b244daae237892d880c8da33dca04e04136ee79
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2023-07-24 18:59:00 -0400

    Link fake database to API

---
+++ PP.APi/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PP.APi.Database;
+using PP.Library.Models;
+
+namespace PP.APi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Client> Get()
+        {
+            return ClientDatabase.clients;
+        }
+    }
+
+
+}
