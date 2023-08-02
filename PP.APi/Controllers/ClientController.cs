31bcab51dd47c1ad00bbbfbb3315b1854cd5a1dd
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2023-08-02 19:21:54 -0400

    Add persistance functionality

---
+++ PP.APi/Controllers/ClientController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PP.APi.Database;
+using PP.APi.EC;
+using PP.Library.Models;
+
+namespace PP.APi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Client> Get()
+        {
+            return Filebase.Current.Clients;
+        }
+
+        [HttpGet("/{id}")]
+        public Client GetByID(int id) 
+        {
+           return new ClientEC().GetByID(id);
+        }
+
+        [HttpDelete("/Delete/{id}")]
+        public Client Delete(int id)
+        {
+           return new ClientEC().Delete(id);
+        }
+
+        [HttpPost("/Edit/{id}")]
+        public Client Edit([FromBody] Client client)
+        {
+            return new ClientEC().Edit(client);
+        }
+
+
+        [HttpPost("/Add")]
+        public Client Add([FromBody] Client client)
+        {
+            return new ClientEC().Add(client);
+        }
+
+    }
+
+
+}
---
+++ PP.APi/EC/ClientEC.cs
@@ -0,0 +1,45 @@
+﻿using PP.APi.Database;
+using PP.Library.Models;
+
+namespace PP.APi.EC
+{
+    public class ClientEC
+    {
+        public Client Edit (Client client)
+        {
+            var clientToEdit 
+                = Filebase.Current.Clients[client.Id];
+
+            if(clientToEdit != null)
+            {
+                Filebase.Current.Clients.Remove(clientToEdit);
+            }
+            Filebase.Current.AddOrUpdate(client);
+            return client;
+        }
+
+        public Client Add (Client client) 
+        {
+            client.Id = Filebase.NextId;
+            Filebase.Current.AddOrUpdate(client);
+            return client;
+        }
+
+        public Client Delete(int id)
+        {
+            if (id < 0 || id > Filebase.Current.Clients.Count)
+                return new Client();
+            var ClientToDelete = Filebase.Current.Clients[id];
+            Filebase.Current.Delete(id.ToString());
+            return ClientToDelete;
+        }
+
+        public Client GetByID(int id)
+        {
+            if (id < 0 || id > Filebase.Current.Clients.Count)
+                return new Client();
+            return Filebase.Current.Clients[id];
+        }
+    }
+}
+
